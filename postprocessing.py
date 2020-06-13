a793752cf2d2db68fad9b5ccd59f58a7a17c876b
TheresaV <kiwi16@aon.at>
2020-06-13 12:40:46 +0200

    eine falsche Klammer ausgebessert, und einen Faktor 1/2, die Parameter sind noch die von Gruppe 4

---
+++ postprocessing.py
@@ -0,0 +1,10 @@
+from elementtest import *
+import matplotlib.pyplot as plt
+
+#useful data
+coordinates=nodeToCoordinate((np.array(range(N*N)).reshape((N,N))),N,L)
+
+
+
+plt.figure()
+#plt.contour(coordinates,sol.reshape((N,N)))
